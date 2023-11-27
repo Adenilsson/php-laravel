0352a0dcba62a1d145cfcdf34137cd5e610ef22d
Adenilsson <adenilson@fairtek.com.br>
2023-11-27 16:29:22 -0300

    cadastro de imagens aula17

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -8,21 +8,55 @@ use App\Models\Event;
 
 class EventController extends Controller
 {
-    public function index(){
+    
+    public function index() {
+
         $events = Event::all();
-        return view('welcome',['events'=>$events]);
+    
+        return view('welcome',['events' => $events]);
+
     }
-    public function create(){
+
+    public function create() {
         return view('events.create');
     }
-    public function store(Request $request){
-        echo "estou dentro do eventController";
+
+    public function store(Request $request) {
+
         $event = new Event;
+
         $event->title = $request->title;
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
+
+        // Image Upload
+        if($request->hasFile('image') && $request->file('image')->isValid()) {
+
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+
+            $requestImage->move(public_path('img/events'), $imageName);
+
+            $event->image = $imageName;
+
+        }
+
         $event->save();
-        return redirect('/');
+
+        return redirect('/')->with('msg', 'Evento criado com sucesso!');
+
     }
+
+    public function show($id) {
+
+        $event = Event::findOrFail($id);
+
+        return view('events.show', ['event' => $event]);
+        
+    }
+
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,13 +10,13 @@
         <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
     </for>
 </div>
-<div id="events-container" class="row">
+<div id="events-container" class="col-md-12">
     <h2>Próximos Eventos</h2>
     <p class="subtitle"> Veja os eventos dos próximos dias</p>
     <div id="cards-container" class="row">
         @foreach($events as $event)
         <div class="card col-md-3">
-            <img src="img/OIP.jpg" alt="{{$event->title}}">
+            <img src="img/events/{{$event->image}}" alt="{{$event->title}}">
             <div class="card-body">
                 <p class="card-date">10-09-2023</p>
                 <h5 class="card-title">{{$event->title}}</h5>
